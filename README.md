c1e9c80a94bde2268522f623b3fe27edd421320b
Markus Hölzle <maxwiese13@gmail.com>
2019-12-18 16:15:10 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Magges geilster Youtube-Downloader
+meine Implementation von Convert2mp3.net basierent auf youtube-dl und Flask
+
+### Requirements
+* Python3
+
+### Installation
+* lade dieses Repository herunter
+* installiere die Python Requirements mit `pip install -r requirements.txt`
+* starte den Server mit `python3 app.py`
+
+## Authors
+
+* **Markus Hölzle** - *Initial work* - [maxwiese](https://github.com/maxwiese)
+
+
---
+++ app.py
@@ -0,0 +1,69 @@
+from __future__ import unicode_literals
+from flask import Flask, render_template, request, send_from_directory, current_app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import youtube_dl
+
+
+class SearchBarForm(FlaskForm):
+    search_term = StringField('SearchTerm', validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchBarForm()
+
+    if form.is_submitted():
+        search_term = form.search_term.data
+        search_result = requests.get(
+            f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=42&q={search_term}&key=AIzaSyAYTvVEGzHX3AtoNAaojZIg62M3WQ-yZhE')
+        items = search_result.json()['items']
+
+        return render_template('results.html', result=items)
+
+    return render_template('index.html', form=form)
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    return render_template('results.html')
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    url = request.form['url']
+
+    ydl_opts = {
+        'writethumbnail': True,
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'},
+            {'key': 'EmbedThumbnail'}],
+        'call_home': False,
+        'progress_hooks': [pg_hook],
+        'outtmpl': '%(title)s.%(ext)s'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+        file = filename[:-4] + "mp3"
+
+    return send_from_directory(directory=current_app.root_path, filename=file, as_attachment=True)
+
+
+def pg_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
